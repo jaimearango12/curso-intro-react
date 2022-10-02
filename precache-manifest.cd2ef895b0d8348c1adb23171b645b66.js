17035d6953abd71cbecda201e3d91fe7bafc7219
jaimearango12 <jarango_12@hotmail.com>
2022-10-02 18:20:45 -0500

    Updates

---
+++ precache-manifest.cd2ef895b0d8348c1adb23171b645b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e6292a34764f60166c7",
+    "url": "/curso-intro-react/static/css/main.5e6d617b.chunk.css"
+  },
+  {
+    "revision": "6e6292a34764f60166c7",
+    "url": "/curso-intro-react/static/js/main.6e6292a3.chunk.js"
+  },
+  {
+    "revision": "aa231f3c252285b57ddc",
+    "url": "/curso-intro-react/static/js/1.aa231f3c.chunk.js"
+  },
+  {
+    "revision": "8f31ac8995903ac72b62",
+    "url": "/curso-intro-react/static/js/runtime~main.8f31ac89.js"
+  },
+  {
+    "revision": "89c6e5ce793d1e91617267cc85acb83f",
+    "url": "/curso-intro-react/static/media/check.89c6e5ce.svg"
+  },
+  {
+    "revision": "abb8a771e188ec9fd23ba3ef323d6f24",
+    "url": "/curso-intro-react/static/media/delete.abb8a771.svg"
+  },
+  {
+    "revision": "e084293e3a55b228bbad03f737a87ebe",
+    "url": "/curso-intro-react/index.html"
+  }
+];
